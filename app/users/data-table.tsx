10921762c6d2f6d7606eb6578f495ccc8c8ada13
Ghasemifard <125352858+Ghasemifard@users.noreply.github.com>
2023-12-11 20:09:18 +0330

    add search filter for first name

--- app/users/data-table.tsx
+++ app/users/data-table.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import {FiEdit, FiTrash2 } from "react-icons/fi";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 
 import {
   ColumnDef,
   flexRender,
+  SortingState,
+  ColumnFiltersState,
   getCoreRowModel,
   getSortedRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   useReactTable,
-  SortingState,
 } from "@tanstack/react-table";
 
 interface DataTableProps<TData, TValue> {
@@ -23,21 +25,36 @@ export function DataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const table = useReactTable({
     data,
     columns,
-    state:{
-        sorting
+    state: {
+      sorting,
+      columnFilters,
     },
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel:getSortedRowModel(),
+    getSortedRowModel: getSortedRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
     <>
+    {/*  */}
+    <div className="flex items-center py-4">
+        <input
+          placeholder="Filter first name..."
+          value={(table.getColumn("first_name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("first_name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
       {/* Table */}
       <div className="rounded-md border">
         <table>
@@ -65,17 +82,18 @@ export function DataTable<TData, TValue>({
                 <tr key={row.id} data-state={row.getIsSelected() && "selected"}>
                   {row.getVisibleCells().map((cell, index) => (
                     <td key={cell.id}>
-                        {index === row.getVisibleCells().length -1 ?(
-                            // If it's the last column, render the trash icon
-                            <div className="flex"><FiEdit /> <FiTrash2 /></div>
-                        ):(
-                            // Render cell content
-                            flexRender(
-                                cell.column.columnDef.cell,
-                                cell.getContext()
-                              )
-                        )}
-                      
+                      {index === row.getVisibleCells().length - 1 ? (
+                        // If it's the last column, render the trash icon
+                        <div className="flex">
+                          <FiEdit /> <FiTrash2 />
+                        </div>
+                      ) : (
+                        // Render cell content
+                        flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )
+                      )}
                     </td>
                   ))}
                 </tr>
