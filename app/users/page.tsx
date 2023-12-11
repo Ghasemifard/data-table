74ae3421b0a7136af43531417b428fb9ce6da80e
Ghasemifard <125352858+Ghasemifard@users.noreply.github.com>
2023-12-11 13:40:06 +0330

    Install tanstack/react-table and create user page.

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const userPage = () => {
+  return (
+    <section className='p-5'>
+        <div className='container'>
+            <h1 className='text-3xl font-bold'>All Users</h1>
+        </div>
+    </section>
+  )
+}
+
+export default userPage
