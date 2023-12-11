7d9466ad08850e33c544db93d0ea7ac408163847
Ghasemifard <125352858+Ghasemifard@users.noreply.github.com>
2023-12-11 15:58:14 +0330

    Change userPage function to async and fetch data from mocki.io and show them with DataTable component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { User,columns } from "./columns";
+import { DataTable } from "./data-table";
 
-const userPage = () => {
-  return (
-    <section className='p-5'>
-        <div className='container'>
-            <h1 className='text-3xl font-bold'>All Users</h1>
-        </div>
-    </section>
-  )
+async function getUsers():Promise<User[]> {
+    const res = await fetch(
+        'https://mocki.io/v1/3361b3fd-79ad-45c2-aba4-3ee66cc9230c'
+    )
+    const data = await res.json()
+    return data
 }
+export default async function userPage() {
+    const data = await getUsers()
 
-export default userPage
+  return (
+    <section className="p-5">
+      <div className="container">
+        <h1 className="text-3xl font-bold">All Users</h1>
+        <DataTable columns={columns} data={data}/>
+      </div>
+    </section>
+  );
+}
